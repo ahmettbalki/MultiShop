f012184bf1e38764e1095999d41d96c856268556
ahmettbalki <ahmettbalki@gmail.com>
2024-07-01 01:44:00 +0300

    Added to UI section.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_ProductListPaginationComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListPaginationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
