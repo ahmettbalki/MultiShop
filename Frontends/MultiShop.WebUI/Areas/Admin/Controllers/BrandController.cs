bbdfb6525811ca16900450b283a54c8f5ae41c0f
ahmettbalki <ahmettbalki@gmail.com>
2024-07-11 18:55:29 +0300

    Product operations

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.BrandDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    [AllowAnonymous]
+    public class BrandController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BrandController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Markalar";
+            ViewBag.v3 = "Marka Listesi";
+            ViewBag.v0 = "Marka İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7257/api/Brands");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+                return View(values);
+            }
+            ModelState.AddModelError("", "Failed to load categories.");
+            return View();
+        }
+
+        [HttpGet("CreateBrand")]
+        public IActionResult CreateBrand()
+        {
+            return View();
+        }
+
+        [HttpPost("CreateBrand")]
+        public async Task<IActionResult> CreateBrand(CreateBrandDto createBrandDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createBrandDto);
+                var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("https://localhost:7257/api/Brands", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Failed to create Brand.");
+            }
+            return View(createBrandDto);
+        }
+
+        [HttpGet("DeleteBrand/{id}")]
+        public async Task<IActionResult> DeleteBrand(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7257/api/Brands?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Failed to delete Brand.");
+            return View();
+        }
+
+        [HttpGet("UpdateBrand/{id}")]
+        public async Task<IActionResult> UpdateBrand(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7257/api/Brands/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateBrandDto>(jsonData);
+                return View(values);
+            }
+            ModelState.AddModelError("", "Failed to load Brand.");
+            return View();
+        }
+
+        [HttpPost("UpdateBrand/{id}")]
+        public async Task<IActionResult> UpdateBrand(UpdateBrandDto updateBrandDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(updateBrandDto);
+                var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PutAsync("https://localhost:7257/api/Brands/", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Failed to update Brand.");
+            }
+            return View(updateBrandDto);
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/OfferDiscountController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.OfferDiscountDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    [AllowAnonymous]
+    public class OfferDiscountController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OfferDiscountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "İndirim Teklifleri";
+            ViewBag.v3 = "İndirim Teklif Listesi";
+            ViewBag.v0 = "İndirim Teklif İşlemleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7257/api/OfferDiscounts");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultOfferDiscountDto>>(jsonData);
+                return View(values);
+            }
+            ModelState.AddModelError("", "Failed to load categories.");
+            return View();
+        }
+
+        [HttpGet("CreateOfferDiscount")]
+        public IActionResult CreateOfferDiscount()
+        {
+            return View();
+        }
+
+        [HttpPost("CreateOfferDiscount")]
+        public async Task<IActionResult> CreateOfferDiscount(CreateOfferDiscountDto createOfferDiscountDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createOfferDiscountDto);
+                var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("https://localhost:7257/api/OfferDiscounts", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Failed to create OfferDiscount.");
+            }
+            return View(createOfferDiscountDto);
+        }
+
+        [HttpGet("DeleteOfferDiscount/{id}")]
+        public async Task<IActionResult> DeleteOfferDiscount(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7257/api/OfferDiscounts?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Failed to delete OfferDiscount.");
+            return View();
+        }
+
+        [HttpGet("UpdateOfferDiscount/{id}")]
+        public async Task<IActionResult> UpdateOfferDiscount(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7257/api/OfferDiscounts/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateOfferDiscountDto>(jsonData);
+                return View(values);
+            }
+            ModelState.AddModelError("", "Failed to load OfferDiscount.");
+            return View();
+        }
+
+        [HttpPost("UpdateOfferDiscount/{id}")]
+        public async Task<IActionResult> UpdateOfferDiscount(UpdateOfferDiscountDto updateOfferDiscountDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(updateOfferDiscountDto);
+                var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PutAsync("https://localhost:7257/api/OfferDiscounts/", stringContent);
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Failed to update OfferDiscount.");
+            }
+            return View(updateOfferDiscountDto);
+        }
+    }
+}
