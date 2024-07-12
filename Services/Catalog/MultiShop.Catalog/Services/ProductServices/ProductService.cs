8a81bc5338bf0c0509b0ebee3b25a0324e91ba24
ahmettbalki <ahmettbalki@gmail.com>
2024-07-12 03:53:14 +0300

    Product Operations

--- Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/ProductServices/ProductService.cs
@@ -48,6 +48,16 @@ namespace MultiShop.Catalog.Services.ProductServices
             return _mapper.Map<List<ResultProductsWithCategoryDto>>(values);
         }
 
+        public async Task<List<ResultProductsWithCategoryDto>> GetProductsWithCategoryByCategoryIdAsync(string CategoryId)
+        {
+            var values = await _productCollection.Find(x => x.CategoryId == CategoryId).ToListAsync();
+            foreach (var item in values)
+            {
+                item.Category = await _categoryCollection.Find<Category>(x => x.CategoryID == item.CategoryId).FirstAsync();
+            }
+            return _mapper.Map<List<ResultProductsWithCategoryDto>>(values);
+        }
+
         public async Task<List<ResultProductDto>> GettAllProductAsync()
         {
             var values = await _productCollection.Find(x => true).ToListAsync();
